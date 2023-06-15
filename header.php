9c756740f9688dc8627632618a7df48a1f1dfe76
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-06-15 16:05:12 -0500

    navbar 50%

--- header.php
+++ header.php
@@ -9,14 +9,29 @@
 
 <body>
   <header class="navbar" style="text-align: center;">
-  <img src="http://localhost/WORDPRESS/sanfer/wp-content/uploads/2023/06/Logo_Sanfer.png" alt="logo">
-  <div>
-  <ul class="menu">
-  <li class="menu-item "><a href="default.asp">Home</a></li>
-  <li class="menu-item "><a href="news.asp">News</a></li>
-  <li class="menu-item "><a href="contact.asp">Contact</a></li>
-  <li class="menu-item "><a href="about.asp">About</a></li>
-</ul>
-  </div>
+    <img src="http://localhost/WORDPRESS/sanfer/wp-content/uploads/2023/06/Logo_Sanfer.png" alt="logo" class="logo_sf">
+    <div>
+      <div class="div_menu2">
+      <ul class="menu2">
+        <li class="menu2-item"><a href="#">Nosotros</a></li>
+        <li class="menu2-item"><a href="#">Prensa</a></li>
+        <li class="menu2-item"><a href="#">Sostenibilidad</a></li>
+        <li class="menu2-item"><a href="#">Trabaja con nosotros</a></li>
+      </ul>
+      </div>
+      <div>
+      <ul class="menu">
+        <li class="menu-item"><a href="#">Campañas</a></li>
+        <li class="menu-item"><a href="#">Productos</a></li>
+        <li class="menu-item">Blog Bien Servido</li>
+        <li class="menu-item"><a href="#">Contacto</a></li>
+        <li class="menu-item"><button class="btn-vales">Vales <svg width="18" height="11" viewBox="0 0 18 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.986328 2.43591L9.0002 10.4498L17.0141 2.43591L15.1285 0.550294L9.0002 6.67855L2.87195 0.550293L0.986328 2.43591Z" fill="#96DAEA"/>
+</svg></button></li>
+        <li class="menu-item"><button class="btn-comprar">COMPRAR AQUI</button></li>
+      </ul>
+    </div>
+    </div>
     
+
   </header>
