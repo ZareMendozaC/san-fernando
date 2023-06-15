e8e7bda3d5ad3adf6533911d24d2c3e6237b6afe
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-06-15 13:32:47 -0500

    nav

--- header.php
+++ header.php
@@ -8,3 +8,15 @@
 </head>
 
 <body>
+  <header class="navbar" style="text-align: center;">
+  <img src="http://localhost/WORDPRESS/sanfer/wp-content/uploads/2023/06/Logo_Sanfer.png" alt="logo">
+  <div>
+  <ul class="menu">
+  <li class="menu-item "><a href="default.asp">Home</a></li>
+  <li class="menu-item "><a href="news.asp">News</a></li>
+  <li class="menu-item "><a href="contact.asp">Contact</a></li>
+  <li class="menu-item "><a href="about.asp">About</a></li>
+</ul>
+  </div>
+    
+  </header>
