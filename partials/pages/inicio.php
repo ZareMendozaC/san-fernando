9c55ebd15549a9361a64835ee8a40b40058b116a
Fvarasapros <fvaras@apros.pe>
2023-06-15 13:20:31 -0500

    Version1.2

--- partials/pages/inicio.php
+++ partials/pages/inicio.php
@@ -1,31 +1,31 @@
 <?php
-    $slider_banner = get_field("lista_de_imagenes");
+$slider_banner = get_field("lista_de_imagenes");
 ?>
 <section class="main-banner">
     <?php foreach ($slider_banner as $item) : ?>
         <?php
-            $item_image = $item["imagen"];
-            $item_image_url = $item_image ? $item_image["url"] : null;
-            $item_url = $item["link_y_utms"];
-            $item_estado= $item["estado"];
+        $item_image = $item["imagen"];
+        $item_image_url = $item_image ? $item_image["url"] : null;
+        $item_url = $item["link_y_utms"];
+        $item_estado = $item["estado"];
         ?>
         <?php if ($item_url) : ?>
             <a href="<?= $item_url; ?>" target="_blank">
-        <?php endif; ?>
+            <?php endif; ?>
             <div class="container-main bg-main-banner" style="background-image: url(<?= $item_image_url; ?>)">
             </div>
-        <?php if ($item_url) : ?>
+            <?php if ($item_url) : ?>
             </a>
         <?php endif; ?>
     <?php endforeach; ?>
 </section>
 <?php
-    $titulo = get_field("titulo");
-    $imagen_fondo = get_field("imagen_de_fondo");
-    $lista_productos = get_field('listado_de_productos');
+$titulo = get_field("titulo");
+$imagen_fondo = get_field("imagen_de_fondo");
+$lista_productos = get_field('listado_de_productos');
 ?>
 <section>
-    <div class="container-main bg-productos" style="background-image: url(<?php echo $imagen_fondo['url'];?>)">
+    <div class="container-main bg-productos" style="background-image: url(<?php echo $imagen_fondo['url']; ?>)">
         <?php if ($titulo) : ?>
             <div class="content-h2">
                 <h2 class="text-center color-blue"><?= $titulo; ?></h2>
@@ -35,15 +35,15 @@
         <div class="lista-productos">
             <?php foreach ($lista_productos as $item) : ?>
                 <?php
-                    $item_image = $item["imagen_producto"];
-                    $item_image_url = $item_image ? $item_image["url"] : null;
-                    $item_link = $item["url_landing"];
+                $item_image = $item["imagen_producto"];
+                $item_image_url = $item_image ? $item_image["url"] : null;
+                $item_link = $item["url_landing"];
                 ?>
                 <?php if ($item_link) : ?>
                     <a href="<?= $item_url; ?>" target="_blank">
-                <?php endif; ?>
+                    <?php endif; ?>
                     <img style="width: calc(100vw*(230/var(--width_base)));" src="<?= $item_image_url; ?>" alt="">
-                <?php if ($item_link) : ?>
+                    <?php if ($item_link) : ?>
                     </a>
                 <?php endif; ?>
             <?php endforeach; ?>
@@ -54,13 +54,13 @@
     </div>
 </section>
 <?php
-    $titulo_cotiza = get_field('titulo_cotiza');
-    $descripcion_cotiza = get_field('descripcion_cotiza');
-    $opciones_cotiza_y_compra = get_field('opciones_cotiza_y_compra');
+$titulo_cotiza = get_field('titulo_cotiza');
+$descripcion_cotiza = get_field('descripcion_cotiza');
+$opciones_cotiza_y_compra = get_field('opciones_cotiza_y_compra');
 ?>
 <section>
     <div class="container">
-    <?php if ($titulo_cotiza) : ?>
+        <?php if ($titulo_cotiza) : ?>
             <div class="content-h2">
                 <h2 class="text-center color-blue"><?= $titulo_cotiza; ?></h2>
                 <img class="img-sub-2" src="<?php echo home_url(); ?>/wp-content/uploads/2023/06/Frame-16.png" alt="">
