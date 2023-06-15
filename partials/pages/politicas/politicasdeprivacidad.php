bc48a8fb5a6edfd3cadcfec9be0e90fb911dfb38
Fvarasapros <fvaras@apros.pe>
2023-06-15 17:24:08 -0500

    Version 1.5

---
+++ partials/pages/politicas/politicasdeprivacidad.php
@@ -0,0 +1,11 @@
+<section>
+    <div class="politicas-head">
+        <h1 class="color-white zig-zag">Políticas de privacidad</h1>
+    </div>
+
+    <div class="text-politicas container">
+        <?php $text_politicas = get_field("texto_politicas"); ?>
+
+        <?php echo $text_politicas; ?>
+    </div>
+</section>
