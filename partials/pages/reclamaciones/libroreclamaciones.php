346c764f6a3a24deef76e95e245f565e43645ade
Fvarasapros <fvaras@apros.pe>
2023-06-27 10:41:04 -0500

    Version 8.1

--- partials/pages/reclamaciones/libroreclamaciones.php
+++ partials/pages/reclamaciones/libroreclamaciones.php
@@ -10,7 +10,7 @@ $fechaActual = date("d/m/Y");
     <h1>Libro de Reclamaciones</h1>
   </div>
 
-  <div class="subtitle-date container">
+  <div class="subtitle-date">
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
         <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
@@ -20,7 +20,7 @@ $fechaActual = date("d/m/Y");
     <p>Para poder ayudarte, por favor, brindanos tus datos personales</p>
   </div>
 
-  <div class="formulario-recla container">
+  <div class="formulario-recla">
     <?php echo do_shortcode('[contact-form-7 id="435" title="Libro de Reclamaciones"]'); ?>
   </div>
 
