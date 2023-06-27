6b6a37bb9a4f5a2c237d06f26ac7c91f1e48aa28
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-06-27 08:19:08 -0500

    changes

--- partials/pages/nosotros.php
+++ partials/pages/nosotros.php
@@ -67,23 +67,43 @@ $archivos = get_field("archivos");
                 <li class="li-time" style="position:relative;">
                     <span class="timeline-point"></span>
                     <span class="date" style="display:none;"><?= $anio; ?></span>
-                    <div class="legend" >
-     
-                    <img class="img-legend" src="<?= wp_get_attachment_url($foto) ; ?>" alt="foto" style="display:<?= (!wp_get_attachment_url($foto))?'none':'' ; ?>">
-                    <!-- <div class="img-legend"><?= wp_get_attachment_url($foto) ; ?></div>  -->
-                    <div class="p-titleline color-blue">
-                    <?= $anio; ?>
-                    </div>
-                    <div>
-                    <!-- <img class="img-legend" src="https://img.freepik.com/vector-premium/imagen-dibujos-animados-hongo-palabra-hongo_587001-200.jpg" alt="foto" > -->
-                    <p class="tex-legend p-textline color-gris"><?= $leyenda; ?></p> 
-                    </div>
+                    <div class="legend">
+
+                        <img class="img-legend" src="<?= wp_get_attachment_url($foto); ?>" alt="foto" style="display:<?= (!wp_get_attachment_url($foto)) ? 'none' : ''; ?>">
+                        <!-- <div class="img-legend"><?= wp_get_attachment_url($foto); ?></div>  -->
+                        <div class="p-titleline color-blue">
+                            <?= $anio; ?>
+                        </div>
+                        <div>
+                            <!-- <img class="img-legend" src="https://img.freepik.com/vector-premium/imagen-dibujos-animados-hongo-palabra-hongo_587001-200.jpg" alt="foto" > -->
+                            <p class="tex-legend p-textline color-gris"><?= $leyenda; ?></p>
+                        </div>
                     </div>
                 </li>
             <?php endforeach; ?>
         </ul>
-            </div>
+    </div>
 </section>
+<!-- <section class="time-line2 container">
+    <div class="line-time">
+        <ul class="time-ul">
+            <?php foreach ($listado_timeline as $time) : ?>
+                <?php
+                $foto = $time['foto'];
+                $anio = $time['anio'];
+                $leyenda = $time['leyenda'];
+                ?>
+                <li class="time-li">
+
+
+                    <div class="p-titleline color-blue" style="padding:20px; background:red; width:358px;">
+                       <p> <?= $leyenda; ?></p>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>  
+</section> -->
 <section class="section_MV_valores container">
 
     <div class="container_misionvision">
