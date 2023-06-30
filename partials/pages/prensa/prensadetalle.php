49c8bcd288360a2182fc901cb846f56eb4b88398
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-06-30 12:53:57 -0500

    detalle prensa

--- partials/pages/prensa/prensadetalle.php
+++ partials/pages/prensa/prensadetalle.php
@@ -1,26 +1,32 @@
+<?php
+    $id = $_GET['id'];
+
+    $args = array(
+        'post_type' => 'post',
+        'p' => $id
+    );
+    
+    $query = new WP_Query($args);
+    
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            // $thumbnail_url = get_the_post_thumbnail_url();
+            // the_title();
+            // the_content();
+            // ... y otros campos del post que necesites            
+        }
+        ?>
 <section class="section-prensa">
     <div class="trabaja-head">
         <h1 class="color-white">Prensa</h1>
     </div>
     <div class="container d-flex content-prensa">
         <div class="prensa-1">
-            <h2 class="color-blue">Nace Hy-Line Perú: alianza estratégica en beneficio de productores de huevo en Perú </h2>
-            <div class="img-destacada" style="background: url(<?php echo home_url(); ?>/wp-content/uploads/2023/06/image-131.png);"></div>
+            <h2 class="color-blue"><?= get_the_title(); ?></h2>
+            <div class="img-destacada" style="background: url(<?= get_the_post_thumbnail_url(); ?>);"></div>
             <div class="texto-prensa">
-            Se fortalece oferta de pollitas para productores de huevos en Perú
-            <br>
-            Hy-Line Perú está formada por International Layer Distribution (ILD), división del Grupo EW, y San Fernando, empresas de reconocido prestigio internacional.
-            La alianza permitirá fortalecer la distribución y asesoramiento a productores locales.
-            <br>
-            Lima, julio de 2022. International Layer Distribution (ILD), división del Grupo EW dedicada a coordinar empresas de distribución de pollitas a los productores de huevos, firmó un acuerdo estratégico con San Fernando, a través de su división de genética Produss, distribuidora exclusiva de las estirpes Hy-Line hace casi 40 años en Perú, para que, a partir de 1er de julio de 2022, se cree la nueva empresa “Hy-Line Perú”, dándole continuidad al exitoso trabajo de distribución de pollitas Hy-Line al mercado local.
-            <br>
-            Para Luis Raúl Puiggros, gerente general de San Fernando, “esta transacción traerá valor adicional a los productores de huevo en Perú, ya que asociándonos a este grupo genético especializado, reforzaremos la posición de mercado global de Hy-Line, aumentando su participación en el país con productos de alta calidad y excepcional desempeño productivo”.
-            <br>
-            Por su parte, Tiago Lourenco, director de ILD LATAM comenta: “Estamos entusiasmados de que esta nueva empresa ahora pase al control de un grupo alemán en sociedad con un grupo peruano de gran trayectoria. Hy-Line Perú se ha creado para fortalecer nuestras operaciones de distribución en Latinoamérica dentro del Grupo EW, con el propósito de brindar productos de alta calidad y asesoramiento en beneficio de los productores peruanos”.
-            <br>
-            El Ing. Santiago Ágreda, con una sólida trayectoria dedicada al sector avícola y que participó de la creación de la división genética de Produss de San Fernando desde sus inicios, ha sido nombrado gerente general de Hy-Line Perú, y estará a cargo de liderar la alianza estratégica de estos dos grandes grupos y fortalecer aún más la relación del mercado con la compañía.
-            <br>
-            “Hy-Line Perú tiene los productos adecuados que el mercado peruano demanda y, además, los valores alineados y compartidos con nuestros clientes y colaboradores. Estamos fuertemente comprometidos en mejorar nuestros procesos productivos y servicios al productor, agregando valor, información y rentabilidad al sector de producción de huevo peruano”, finaliza el Ing. Ágreda.
+            <?= get_the_content(); ?>
             </div>
         </div>
         <div class="col-des-menu-1 prensa-2">
@@ -28,7 +34,7 @@
                 <div class="select-title">
                     <p class="color-blue">Nosotros</p>
                     <svg class="close-des" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.28921 15.7901C5.38217 15.8839 5.49277 15.9583 5.61463 16.009C5.73649 16.0598 5.8672 16.0859 5.99921 16.0859C6.13122 16.0859 6.26192 16.0598 6.38378 16.009C6.50564 15.9583 6.61624 15.8839 6.70921 15.7901L11.2892 11.2101C11.3822 11.1164 11.4928 11.042 11.6146 10.9912C11.7365 10.9405 11.8672 10.9143 11.9992 10.9143C12.1312 10.9143 12.2619 10.9405 12.3838 10.9912C12.5056 11.042 12.6162 11.1164 12.7092 11.2101L17.2892 15.7901C17.3822 15.8839 17.4928 15.9583 17.6146 16.009C17.7365 16.0598 17.8672 16.0859 17.9992 16.0859C18.1312 16.0859 18.2619 16.0598 18.3838 16.009C18.5056 15.9583 18.6162 15.8839 18.7092 15.7901C18.8955 15.6028 19 15.3493 19 15.0851C19 14.821 18.8955 14.5675 18.7092 14.3801L14.1192 9.79014C13.5567 9.22834 12.7942 8.91278 11.9992 8.91278C11.2042 8.91278 10.4417 9.22834 9.87921 9.79014L5.28921 14.3801C5.10296 14.5675 4.99841 14.821 4.99841 15.0851C4.99841 15.3493 5.10296 15.6028 5.28921 15.7901Z" fill="#004A96"/>
+                        <path d="M5.28921 15.7901C5.38217 15.8839 5.49277 15.9583 5.61463 16.009C5.73649 16.0598 5.8672 16.0859 5.99921 16.0859C6.13122 16.0859 6.26192 16.0598 6.38378 16.009C6.50564 15.9583 6.61624 15.8839 6.70921 15.7901L11.2892 11.2101C11.3822 11.1164 11.4928 11.042 11.6146 10.9912C11.7365 10.9405 11.8672 10.9143 11.9992 10.9143C12.1312 10.9143 12.2619 10.9405 12.3838 10.9912C12.5056 11.042 12.6162 11.1164 12.7092 11.2101L17.2892 15.7901C17.3822 15.8839 17.4928 15.9583 17.6146 16.009C17.7365 16.0598 17.8672 16.0859 17.9992 16.0859C18.1312 16.0859 18.2619 16.0598 18.3838 16.009C18.5056 15.9583 18.6162 15.8839 18.7092 15.7901C18.8955 15.6028 19 15.3493 19 15.0851C19 14.821 18.8955 14.5675 18.7092 14.3801L14.1192 9.79014C13.5567 9.22834 12.7942 8.91278 11.9992 8.91278C11.2042 8.91278 10.4417 9.22834 9.87921 9.79014L5.28921 14.3801C5.10296 14.5675 4.99841 14.821 4.99841 15.0851C4.99841 15.3493 5.10296 15.6028 5.28921 15.7901Z" fill="#004A96" />
                     </svg>
                 </div>
                 <div class="select-options">
@@ -41,7 +47,7 @@
                 <div class="select-title">
                     <p class="color-blue">Sostenibilidad</p>
                     <svg class="close-des" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.28921 15.7901C5.38217 15.8839 5.49277 15.9583 5.61463 16.009C5.73649 16.0598 5.8672 16.0859 5.99921 16.0859C6.13122 16.0859 6.26192 16.0598 6.38378 16.009C6.50564 15.9583 6.61624 15.8839 6.70921 15.7901L11.2892 11.2101C11.3822 11.1164 11.4928 11.042 11.6146 10.9912C11.7365 10.9405 11.8672 10.9143 11.9992 10.9143C12.1312 10.9143 12.2619 10.9405 12.3838 10.9912C12.5056 11.042 12.6162 11.1164 12.7092 11.2101L17.2892 15.7901C17.3822 15.8839 17.4928 15.9583 17.6146 16.009C17.7365 16.0598 17.8672 16.0859 17.9992 16.0859C18.1312 16.0859 18.2619 16.0598 18.3838 16.009C18.5056 15.9583 18.6162 15.8839 18.7092 15.7901C18.8955 15.6028 19 15.3493 19 15.0851C19 14.821 18.8955 14.5675 18.7092 14.3801L14.1192 9.79014C13.5567 9.22834 12.7942 8.91278 11.9992 8.91278C11.2042 8.91278 10.4417 9.22834 9.87921 9.79014L5.28921 14.3801C5.10296 14.5675 4.99841 14.821 4.99841 15.0851C4.99841 15.3493 5.10296 15.6028 5.28921 15.7901Z" fill="#004A96"/>
+                        <path d="M5.28921 15.7901C5.38217 15.8839 5.49277 15.9583 5.61463 16.009C5.73649 16.0598 5.8672 16.0859 5.99921 16.0859C6.13122 16.0859 6.26192 16.0598 6.38378 16.009C6.50564 15.9583 6.61624 15.8839 6.70921 15.7901L11.2892 11.2101C11.3822 11.1164 11.4928 11.042 11.6146 10.9912C11.7365 10.9405 11.8672 10.9143 11.9992 10.9143C12.1312 10.9143 12.2619 10.9405 12.3838 10.9912C12.5056 11.042 12.6162 11.1164 12.7092 11.2101L17.2892 15.7901C17.3822 15.8839 17.4928 15.9583 17.6146 16.009C17.7365 16.0598 17.8672 16.0859 17.9992 16.0859C18.1312 16.0859 18.2619 16.0598 18.3838 16.009C18.5056 15.9583 18.6162 15.8839 18.7092 15.7901C18.8955 15.6028 19 15.3493 19 15.0851C19 14.821 18.8955 14.5675 18.7092 14.3801L14.1192 9.79014C13.5567 9.22834 12.7942 8.91278 11.9992 8.91278C11.2042 8.91278 10.4417 9.22834 9.87921 9.79014L5.28921 14.3801C5.10296 14.5675 4.99841 14.821 4.99841 15.0851C4.99841 15.3493 5.10296 15.6028 5.28921 15.7901Z" fill="#004A96" />
                     </svg>
                 </div>
                 <div class="select-options">
@@ -55,3 +61,13 @@
         </div>
     </div>
 </section>
+<?php
+    } else {
+        ?>
+        <div style="width:100vw; height:100vh; display:flex; align-items:center; justify-content:center; text-align:center; ">
+        <h2 class="color-blue">No se ha podido acceder<br> los detalles<br> de la noticia seleccionada</h2>
+    </div>
+        <?php
+    }
+    wp_reset_postdata();
+    ?>
